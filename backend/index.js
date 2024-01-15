f5376e673dca45c75162264749bc71b59d641bab
himanshu2168.be21@chitkara.edu.in <himanshu2168.be21@chitkara.edu.in>
2024-01-15 16:52:09 +0530

    new commit

--- backend/index.js
+++ backend/index.js
@@ -2,12 +2,37 @@ const express = require('express');
 const mongoose = require('mongoose');
 require('dotenv').config();
 const cors = require('cors');
-const userRoute = require('./route/userRoute.js');
-const bookRoute = require('./route/bookRoute.js');
 const app = express();
 const corsOptions = {
   origin: '*',
 };
+
+const {
+  bookRoute,
+  booksRoute,
+  categoryRoute,
+  createBook,
+  getAllRentalsRoute,
+  moveToRentedRoute,
+  requestedBooksRoute,
+  requestRoute,
+  returnedStatChange,
+} = require( "./controller/bookController.js");
+
+
+const{
+  addFavourites,
+  deleteFavourites,
+  deleteUser,
+  getBookInHand,
+  getFavourites,
+  getUser,
+  getUsers,
+  loginRoute,
+  registerRoute,
+  updateUser,
+}= require ("./controller/userController.js");
+
 
 const mongo = async () => {
   try {
@@ -22,8 +47,6 @@ const mongo = async () => {
 // MIDDLEWARES.
 app.use(cors(corsOptions));
 app.use(express.json()); // To parse JSON objects.
-app.use('/user', userRoute); // User routes.
-app.use('/books', bookRoute); // Book routes.
 
 app.use((err, req, res, next) => {
   res.status(err.status || 500).json({
@@ -33,6 +56,70 @@ app.use((err, req, res, next) => {
     stack: err.stack,
   });
 });
+// book routes
+app.get("/books/", booksRoute);
+
+// Get a specific book by ID
+app.get("/books/:id", bookRoute);
+
+// Get all books of a specific type/category
+app.get("/books/all/:type", categoryRoute);
+
+// Create a new book
+app.post("/books/create", createBook);
+
+// Request a book from admin
+app.post("/books/request", requestRoute);
+
+// Get all requested books
+app.get("/books/request/all", requestedBooksRoute);
+
+// Get all rented books
+app.get("/books/rentals/all", getAllRentalsRoute);
+
+// Move a requested book to rented status
+app.post("/books/request/one", moveToRentedRoute);
+
+// Update the status when a book is returned
+app.put("/books/returned/update", returnedStatChange);
+
+
+
+// user routes
+app.post("/user/register", registerRoute);
+
+// Log in a user
+app.post("/user/login", loginRoute);
+
+// Get a specific user by ID
+app.get("/user/:id", getUser);
+
+// Get all users
+app.get("/user/", getUsers);
+
+// Update user information
+app.put("/user/edit", updateUser);
+
+// Delete a user by ID
+app.delete("/user/delete/:id", deleteUser);
+
+// Add a book to user's favorites
+app.put("/user/addFav", addFavourites);
+
+// Get a user's favorite books
+app.get("/user/getfav/:id", getFavourites);
+
+// Remove a book from user's favorites
+app.put("/user/deletefav", deleteFavourites);
+
+// Get information about books in hand, rented history, and requested books for a user
+app.get("/user/get/bookinhand/:id", getBookInHand);
+
+
+
+
+
+
 
 app.get('/', (req, res) => {
   return res.json('Hello, this is your API!');
